f7073bbc0e3e85aa6647e5712529ed5ed103f5ee
waterFly <yiyeff@gmail.com>
2021-10-04 10:28:47 +0800

    字符串的扩展

---
+++ Code/_03_字符串的扩展.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: WaterFly
+ * @Date: 2021-10-04 10:25:41
+ * @Description: 字符串的扩展
+ * Just enjoy code.
+ */
+
+/*
+字符串内包含变量，使用 ${} 包括
+
+*/
+{
+  let name = "ZhangSan";
+  let age = 18;
+  let detail = `${name} is ${age}`;
+  console.log(detail);
+}
