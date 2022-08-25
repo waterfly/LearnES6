c2d9c5451572c29f475701c7c47e1a488dfae4c7
waterFly <yiyeff@gmail.com>
2022-08-25 19:58:35 +0800

    Add Generator

---
+++ Code/_19_Generator 函数的语法.js
@@ -0,0 +1,61 @@
+{
+  // 1. Generator 函数基本写法
+  {
+    function* helloWorldGenerator() {
+      console.log("hello");
+      yield "hello";
+
+      console.log("world");
+      yield "world";
+
+      console.log("ending");
+      return "ending";
+    }
+
+    // 返回一个 遍历器对象（Iterator Object）
+    var hw = helloWorldGenerator();
+
+    // 第一种写法，next函数
+    {
+      hw.next(); // {value: 'hello', done: false}
+      hw.next(); // {value: 'world', done: false}
+      hw.next(); // {value: 'ending', done: true}
+    }
+
+    // 第二种写法，使用 for...of ，注意 hw 遍历器对象只能遍历一次，遍历完以后就不能再遍历
+    {
+      for (const item of hw) {
+        console.log(item);
+      }
+    }
+  }
+
+  // 2. 使用 Generator 生成 Iterator接口
+  {
+    var myIterable = {};
+    // 直接将 Generator 函数赋值给 Symbol.iterator 属性即可
+    myIterable[Symbol.iterator] = function* () {
+      yield 1;
+      yield 2;
+      yield 3;
+    };
+
+    // myIterable 即可具备 Iterator 接口
+    console.log([...myIterable]);
+  }
+
+  // 3. yield表达式的值会作为 next函数的返回值，next函数的参数或作为上一个yield表达式的值
+  {
+    function* foo(x) {
+      var y = 3 * (yield x + 1); // 只执行到 yield x + 1 表达式，并返回 x + 1的结果
+      var z = yield y / 3; // 先执行 y = 3 * next函数的传参（注意，需要把 yield x + 1 替换为 next函数的参数，并不是表达式本身的结果），然后执行  y / 3；
+      return x + y + z;
+    }
+
+    // 使 x = 4
+    var b = foo(4);
+    console.log(b.next()); // { value:6, done:false }
+    console.log(b.next(4)); // { value:4, done:false }
+    console.log(b.next(13)); // { value:29, done:true }
+  }
+}
